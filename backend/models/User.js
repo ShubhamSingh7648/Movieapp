38e5e269b2786d774d75688aef1fb6146c0154e3
shubham7648 <shubhams7648@gmail.com>
2025-12-14 12:23:32 +0530

    final fixed mongoose error

--- backend/models/User.js
+++ backend/models/User.js
@@ -18,41 +18,42 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 6,
-    select: false // Don't return password by default
+    select: false
   },
   profilePicture: {
     type: String,
     default: 'https://via.placeholder.com/150'
   },
-  favorites: [{
-    imdbID: String,
-    Title: String,
-    Year: String,
-    Poster: String,
-    Type: String,
-    addedAt: {
-      type: Date,
-      default: Date.now
+  favorites: [
+    {
+      imdbID: String,
+      Title: String,
+      Year: String,
+      Poster: String,
+      Type: String,
+      addedAt: {
+        type: Date,
+        default: Date.now
+      }
     }
-  }],
+  ],
   createdAt: {
     type: Date,
     default: Date.now
   }
 });
 
-// Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  
+// ✅ FIXED: async hook WITHOUT next()
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Compare password method
-userSchema.methods.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
