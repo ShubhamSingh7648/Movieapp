14fbc295d744ea2606cdcc840ab9b5c60349ecda
shubham7648 <shubhams7648@gmail.com>
2026-01-07 21:21:56 +0530

    adding some additional functionality

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ const OMDB_BASE_URL = "https://www.omdbapi.com/";
 
 // ================= BACKEND API =================
 const API_BASE_URL =
-  // import.meta.env.VITE_API_URL || "https://movieapp-1-2flz.onrender.com";
-  import.meta.env.VITE_API_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_URL || "https://movieapp-1-2flz.onrender.com";
+  // import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 // ================= AUTH TOKEN =================
 const getAuthToken = () => {
